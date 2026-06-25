6be0f5394cad95d5677e764d9ef39c4962f8c373
Eduardo Aispuro <eduardo@crownxpress.com>
2026-06-25 15:30:16 -0700

    fix: usar casting a bytea para forzar PDF como binary desde Neon

--- api/inspections/[id].js
+++ api/inspections/[id].js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
       }
 
       const [inspection] = await sql`
-        SELECT pdf_filename, pdf_data, trailer_number, seal_number, lock_number, driver_name, location, inspection_date, inspection_type, trailer_type, guard_name, guard_signature, guard_signed_at, supervisor_name, supervisor_signature, supervisor_signed_at, operator_name, operator_signature, language, tractor_number, container_number, equipment_nomenclature, customer_prefix, odometer, high_security_seal, seal_affixed, wono
+        SELECT pdf_filename, pdf_data::bytea as pdf_data, trailer_number, seal_number, lock_number, driver_name, location, inspection_date, inspection_type, trailer_type, guard_name, guard_signature, guard_signed_at, supervisor_name, supervisor_signature, supervisor_signed_at, operator_name, operator_signature, language, tractor_number, container_number, equipment_nomenclature, customer_prefix, odometer, high_security_seal, seal_affixed, wono
         FROM inspections
         WHERE id = ${inspectionId}
       `
