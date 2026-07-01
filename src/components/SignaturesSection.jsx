a4dbfc23e2ab1c63683570647a8c77fb90ab6904
Eduardo Aispuro <eduardo@crownxpress.com>
2026-07-01 09:05:44 -0700

    Fix SignaturesSection to use correct applicable points for RABON (trailerType check)

--- src/components/SignaturesSection.jsx
+++ src/components/SignaturesSection.jsx
@@ -21,8 +21,9 @@ export default function SignaturesSection() {
   
   // Check if all points are completed
   const applicablePoints = useMemo(() => {
-    return getApplicablePoints(unitInfo?.inspectionType)
-  }, [unitInfo?.inspectionType])
+    const typeToUse = unitInfo?.trailerType === 'RABON' ? 'RABON' : unitInfo?.inspectionType
+    return getApplicablePoints(typeToUse)
+  }, [unitInfo?.inspectionType, unitInfo?.trailerType])
   
   const totalPoints = applicablePoints.length
   const allPointsCompleted = completedCount === totalPoints
